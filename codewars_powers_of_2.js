57591c258968f11b832fb1be7cdede0fe45c6cc7
Crew Spence <csspence123@gmail.com>
2021-04-13 07:12:38 -0500

    Wrote working solution.

--- codewars_powers_of_2.js
+++ codewars_powers_of_2.js
@@ -7,6 +7,18 @@ n = 1  ==> [1, 2]     # [2^0, 2^1]
 n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 */
 
-function powersOfTwo(n){
-  return []
+const powersOfTwo = (n) => {
+  const array = [1];
+  let current = 2;
+  let counter = 1
+  if(n === 0) {
+    return [1];
+  }
+  while(counter < n + 1) {
+    array.push(current);
+    current *= 2;
+    counter++;
+  }
+
+  return array;
 }
